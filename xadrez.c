568134aa49c56db67d74e340bd0082de56bdec0c
Breno <brenool132@gmail.com>
2026-08-28 18:58:19 +0000

    Nivel intermediario concluido

--- xadrez.c
+++ xadrez.c
@@ -5,28 +5,66 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+   // Quantidade de casas que cada peça irá percorrer UTILIZANDO FOR
+   int torre = 5;
+   int bispo = 5;
+   int rainha = 8;
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+  // MOVIMENTO DA TORRE USANDO FOR
+  printf("\nMovimento da torre: \n");
+ 
+  for (int i = 0; i < torre; i++) {
+    printf("Direita\n"); //Imprime a direção do movimento
+  }
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    printf("\n");
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+  //MOVIMENTO DO BISPO UTILIZANDO WHILE
+  printf("\nMovimento do bispo: \n");
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+  int i = 0;
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+  while (i < bispo) {
+    printf("Cima Direita\n");
+    i++;
+  }
+
+    printf("\n");
+
+  // MOVIMENTO DA RAINHA UTILIZANDO DO WHILE
+  printf("\nMovimento Rainha:\n");
+
+  i = 0;
+
+  do {
+    printf("Esquerda\n");
+    i++;
+
+   } while (i < rainha);
+
+    printf("\n");
+
+  // MOVIMENTO DO CAVALO UTILIZANDO FOR E WHILE
+  printf("\nMovimento Cavalo:\n");
+
+    int casasBaixo = 2;
+    int casasEsquerda = 1;
+
+     for (int i = 0; i < casasBaixo; i++) {
+
+        printf("Baixo\n");
+
+        if (i == casasBaixo - 1) {
+
+            int esquerda = 0;
+
+            while (esquerda < casasEsquerda) {
+                printf("Esquerda\n");
+                esquerda++;
+            }
+        }
+    }
 
     return 0;
 }
